3f8e02b2ab3d541bb5f94f5e6625dd9418fed25d
Dassonville Morgane <99723167+Jun080@users.noreply.github.com>
2022-08-24 19:28:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './styles/style.scss';
+
+window.addEventListener('load', function () {
+    console.log('Cette fonction est exécutée une fois quand la page est chargée.');
+});
+
+
+const element = document.getElementById("btn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("balloon").style.width = "300px";
+  document.getElementById("balloon").style.height = "300px";
+  document.getElementById("balloon").style.transition = "all 2s";
+}
